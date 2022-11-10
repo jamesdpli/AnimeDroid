b08322f62b5c667b7efafbf31c34386e888fcda1
James <jamesdudhiprice@gmail.com>
2022-11-10 19:17:54 +0000

    Add retrofit and update data layer

---
+++ app/src/main/java/com/example/animedroid/data/AnimeService.kt
@@ -0,0 +1,11 @@
+package com.example.animedroid.data
+
+import com.example.animedroid.data.response.AnimeResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface AnimeService {
+
+    @GET("anime")
+    suspend fun getAnimes() : Response<AnimeResponse>
+}
