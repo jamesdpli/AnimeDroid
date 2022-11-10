73c6929f34230524a25433ba97b459bfe27875d9
James <jamesdudhiprice@gmail.com>
2022-11-10 19:25:22 +0000

    Set up Retrofit instance

---
+++ app/src/main/java/com/example/animedroid/data/RetrofitInstance.kt
@@ -0,0 +1,15 @@
+package com.example.animedroid.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val BASE_URL: String = "https://kitsu.io/api/edge/"
+
+val retrofit = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+object RetrofitInstance {
+    val animeService = retrofit.create(AnimeService::class.java)
+}
