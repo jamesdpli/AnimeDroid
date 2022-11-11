15517a0accbe9687668ed62977e1cf07788cb2fe
James <jamesdudhiprice@gmail.com>
2022-11-11 14:32:53 +0000

    MVVM basic ui for anime details working

--- app/src/main/java/com/example/animedroid/ui/view/AnimeDetailsFragment.kt
+++ app/src/main/java/com/example/animedroid/ui/view/AnimeDetailsFragment.kt
@@ -5,10 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import coil.load
 import com.example.animedroid.R
+import com.example.animedroid.data.AnimeApi
+import com.example.animedroid.data.AnimesRepository
+import com.example.animedroid.data.RemoteAnimesDataSource
 import com.example.animedroid.databinding.FragmentAnimeDetailsBinding
+import com.example.animedroid.ui.viewmodel.AnimeDetailsViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class AnimeDetailsFragment : Fragment() {
+
+    private val viewModel: AnimeDetailsViewModel by lazy {
+        ViewModelProvider(this)[AnimeDetailsViewModel::class.java]
+    }
 
     private var _binding: FragmentAnimeDetailsBinding? = null
     private val binding get() = _binding!!
@@ -19,6 +32,15 @@ class AnimeDetailsFragment : Fragment() {
     ): View? {
         _binding = FragmentAnimeDetailsBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        viewModel.getAnimes()
+        viewModel.animeLiveData.observe(viewLifecycleOwner) {
+            response ->
+            binding.tvAnimeName.text = response.data[7].attributes.canonicalTitle
+            binding.ivAnimeImage.load(response.data[7].attributes.posterImage.medium)
+            binding.tvAnimeDescription.text = response.data[7].attributes.description
+        }
+
         return view
     }
 
--- app/src/main/java/com/example/animedroid/ui/viewmodel/AnimeDetailsViewModel.kt
+++ app/src/main/java/com/example/animedroid/ui/viewmodel/AnimeDetailsViewModel.kt
@@ -1,6 +1,29 @@
 package com.example.animedroid.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.animedroid.data.AnimeApi
+import com.example.animedroid.data.AnimesRepository
+import com.example.animedroid.data.RemoteAnimesDataSource
+import com.example.animedroid.data.response.AnimeResponse
+import kotlinx.coroutines.launch
 
-class AnimeDetailsViewModel : ViewModel() {
+class AnimeDetailsViewModel: ViewModel() {
+
+    private val repository = AnimesRepository(RemoteAnimesDataSource(AnimeApi))
+
+    private val _animeLiveData = MutableLiveData<AnimeResponse>()
+    val animeLiveData: LiveData<AnimeResponse> = _animeLiveData
+
+    fun getAnimes() {
+        viewModelScope.launch {
+            val response = repository.getAnimes().body()
+
+            _animeLiveData.postValue(response)
+        }
+    }
+
+
 }
