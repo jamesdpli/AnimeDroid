22609bbd27e902c7cb09034322edd62a55d45d0a
James <jamesdudhiprice@gmail.com>
2022-11-12 15:50:56 +0000

    Update AnimeListAdapter

--- app/src/main/java/com/example/animedroid/ui/adapter/AnimeListAdapter.kt
+++ app/src/main/java/com/example/animedroid/ui/adapter/AnimeListAdapter.kt
@@ -1,3 +1,39 @@
-package com.example.animedroid.ui.adapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.animedroid.R
+import com.example.animedroid.data.response.Data
 
-class AnimeListAdapter
+class AnimeListAdapter(private val animeList: List<Data>) :
+    RecyclerView.Adapter<AnimeListAdapter.AnimeViewHolder>() {
+
+    class AnimeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val animeName: TextView
+        val animeImage: ImageView
+
+        init {
+            animeName = view.findViewById(R.id.tvAnimeNameInRv)
+            animeImage = view.findViewById(R.id.ivAnimeImageInRv)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): AnimeViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.anime_recycler_view_item, viewGroup, false)
+
+        return AnimeViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: AnimeViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = animeList.size
+
+}
