03bdf7aa5d3dc76e9113220cce98efbea9d0c687
James <jamesdudhiprice@gmail.com>
2022-12-15 15:42:10 +0000

    Add AppComponent and AnimeApplication

---
+++ app/src/main/java/com/example/animedroid/AnimeApplication.kt
@@ -0,0 +1,12 @@
+package com.example.animedroid
+
+import android.app.Application
+import com.example.animedroid.di.DaggerAppComponent
+
+// appComponent lives in the Application class to share its lifecycle
+class AnimeApplication : Application() {
+
+//  Reference to the application graph that is used across the whole app
+    val appComponent = DaggerAppComponent.create()
+
+}
---
+++ app/src/main/java/com/example/animedroid/di/AppComponent.kt
@@ -0,0 +1,8 @@
+package com.example.animedroid.di
+
+import dagger.Component
+
+// Definition of the Application graph
+@Component
+interface AppComponent {
+}
