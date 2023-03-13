539fb4f1937ee1c2e2c477759945d6d7df05e7bc
James <jamesdudhiprice@gmail.com>
2023-03-13 19:47:41 +0000

    Show progressindicator before anime list

--- app/src/main/java/com/example/animedroid/ui/fragments/AnimeListFragment.kt
+++ app/src/main/java/com/example/animedroid/ui/fragments/AnimeListFragment.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.paging.LoadState
 import com.example.animedroid.data.responses.NetworkAnimeList.Data
 import com.example.animedroid.databinding.FragmentAnimeListBinding
 import com.example.animedroid.ui.adapters.AnimeListAdapter
 import com.example.animedroid.ui.viewmodels.AnimeListFragmentViewModel
 import com.example.animedroid.ui.viewmodels.ViewModelFactory
 import dagger.android.support.DaggerFragment
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,7 +24,8 @@ class AnimeListFragment : DaggerFragment() {
     private var _binding: FragmentAnimeListBinding? = null
     private val binding get() = _binding!!
 
-    @Inject lateinit var viewModelFactory: ViewModelFactory
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
     private val viewModel: AnimeListFragmentViewModel by lazy {
         ViewModelProvider(this, viewModelFactory)[AnimeListFragmentViewModel::class.java]
     }
@@ -37,7 +40,8 @@ class AnimeListFragment : DaggerFragment() {
         _binding = FragmentAnimeListBinding.inflate(layoutInflater, container, false)
         val view = binding.root
         setUpUI()
-        setUpObserver()
+        observeLoadState()
+        observePagedData()
         return view
     }
 
@@ -45,12 +49,17 @@ class AnimeListFragment : DaggerFragment() {
         binding.rvAnimeList.adapter = animeListAdapter
     }
 
-    private fun setUpObserver() {
+    private fun observePagedData() {
         viewModel.pagedAnimeList.observe(viewLifecycleOwner) {
-            pagedAnimeData ->
             lifecycleScope.launch {
-                animeListAdapter.submitData(pagedAnimeData)
+                animeListAdapter.submitData(pagingData = it)
             }
+        }
+    }
+
+    private fun observeLoadState() = lifecycleScope.launch {
+        animeListAdapter.loadStateFlow.collectLatest {
+            binding.recyclerViewProgressBar.isVisible = it.refresh is LoadState.Loading
         }
     }
 
